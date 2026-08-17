1d5750d71d9a46698939c8ef33269b8bf7cfb179
karafarin12800-del <karafarin12800@gmail.com>
2026-08-17 23:12:30 +0330

    renderer: define asset categories

---
+++ app/src/main/java/com/persiawar2d/RenderAssetPolicy.java
@@ -0,0 +1,12 @@
+package com.persiawar2d;
+
+/** Asset categories accepted by the renderer. */
+public enum RenderAssetPolicy {
+    GROUND,
+    ROAD,
+    BUILDING,
+    FOLIAGE,
+    CHARACTER,
+    SHADOW,
+    UI
+}
