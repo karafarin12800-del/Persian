f96faca6d84245dbbc8ebb9ddf19e5c2ae3ff697
karafarin12800-del <karafarin12800@gmail.com>
2026-08-18 09:29:48 +0330

    cleanup: remove unused build verification class

--- app/src/main/java/com/persiawar2d/BuildVerification.java
+++
@@ -1,9 +0,0 @@
-package com.persiawar2d;
-
-public final class BuildVerification {
-    private BuildVerification() {}
-    public static final String VERSION = "2.5D-RENDER-V1";
-    public static final boolean OPENGL_ES_2_ENABLED = true;
-    public static final boolean CHARACTER_SHEET_4X6X3 = true;
-    public static final boolean ORTHOGRAPHIC_CAMERA = true;
-}
