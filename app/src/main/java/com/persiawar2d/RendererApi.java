7c0bae97e2ae7c4d89b0a90406e6caf34af8a096
karafarin12800-del <karafarin12800@gmail.com>
2026-08-18 09:30:51 +0330

    cleanup: remove unused renderer interface

--- app/src/main/java/com/persiawar2d/RendererApi.java
+++
@@ -1,9 +0,0 @@
-package com.persiawar2d;
-
-/** Small renderer-facing contract so gameplay does not know how the world is drawn. */
-public interface RendererApi {
-    void setViewport(int width, int height);
-    void setCamera(float worldX, float worldY, float zoom);
-    void beginFrame();
-    void endFrame();
-}
